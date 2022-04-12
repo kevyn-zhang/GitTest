f7b65b622daa31584adbd6ed3fdf84453711004c
harry <harry@qq.com>
2022-04-12 17:16:20 +0800

    first commit

---
+++ src/main/java/com/zkf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zkf.git;
+
+/**
+ * @author zkf
+ * @Date 2022/4/12 - 17:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
